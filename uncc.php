ccf28141a37d14e128df6c58acf375bf56cf1b28
unknown <juhitha.penumalli@techigai.com>
2024-01-21 23:42:12 -0500

    added index and uncc files

---
+++ uncc.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNCC - Penumalli Juhitha Reddy</title>
+</head>
+<body>
+    <h1>Week 2 - Testing a page</h1>
+    <?php echo 9 * 13?>
+</body>
+</html>
